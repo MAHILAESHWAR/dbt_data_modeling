b2e5adda2df7829948454868041685fe54517b9f
Mahilaeshwar <mahilaesh@gmail.com>
2022-01-29 17:40:32 +0530

    Update COVID19_INDONESIA.sql

--- models/COVID19_INDONESIA.sql
+++ models/COVID19_INDONESIA.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='table') }}
 with source_data as (
-    select *,cast(date as datetime) as date_col from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_MAHILAESHWAR_M"  
+    select *,cast(date as datetime) as date_col,substr(cast(cast(date as datetime) as string),1,7) as month_col from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_MAHILAESHWAR_M"
 )
 select * from source_data
